b1f3672d783919f9a18fb60bd3b068a4c2560e8c
Tom Gooding <tom.pudding@gmail.com>
2020-04-21 09:52:27 +0100

    Downgrade the version

--- keep_the_streak_alive.py
+++ keep_the_streak_alive.py
@@ -68,8 +68,8 @@ def init():
 
     pygame.init()
 
-    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
-    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 1)
+    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 2)
+    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 0)
     pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
 
     screen = pygame.display.set_mode((w, h), pygame.OPENGL | pygame.DOUBLEBUF)
